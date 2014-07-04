262f4361f3580dec6574148b8d3f4e4e8d8d8968
brad henderson <elliotbradly@gmail.com>
2014-07-04 13:12:35 -0700

    it works!!!!!

--- app/globs/globulus.js
+++ app/globs/globulus.js
@@ -18,26 +18,28 @@ var Form = function ( $glob )  {
   }
 
   self.create = function( world ){
+
+    self.score = 0;
+
     world.physics.startSystem(Phaser.Physics.ARCADE);
     world.add.sprite(0, 0, 'sky');
 
-    return;
-    platforms = world.add.group();
+    var platforms = self.platforms = world.add.group();
     platforms.enableBody = true;
 
-    var ground = platforms.create(0, world.world.height - 64, 'ground');
+    var ground = self.ground = platforms.create(0, world.world.height - 64, 'ground');
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
 
-    var ledge = platforms.create(400, 400, 'ground');
+    var ledge = self.ledge = platforms.create(400, 400, 'ground');
     ledge.body.immovable = true;
 
     ledge = platforms.create(-150, 250, 'ground');
     ledge.body.immovable = true;
 
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    var player = self.player = world.add.sprite(32, world.world.height - 150, 'dude');
 
-    game.physics.arcade.enable(player);
+    world.physics.arcade.enable(player);
 
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 300;
@@ -46,7 +48,7 @@ var Form = function ( $glob )  {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    stars = game.add.group();
+    var stars = self.stars = world.add.group();
 
     stars.enableBody = true;
 
@@ -57,17 +59,62 @@ var Form = function ( $glob )  {
         star.body.bounce.y = 0.7 + Math.random() * 0.2;
     }
 
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-    cursors = game.input.keyboard.createCursorKeys();
+    self.scoreText = world.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    self.cursors = world.input.keyboard.createCursorKeys();
   }
 
-  self.update = function(){
+  self.update = function( world ){
     //trace("updating");
+    var player = self.player;
+    var cursors = self.cursors;
+
+    world.physics.arcade.collide( self.player, self.platforms);
+    world.physics.arcade.collide( self.stars, self.platforms);
+    world.physics.arcade.overlap( self.player, self.stars, self.collectStar, null, this);
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
   }
 
   self.render = function(){
     //trace("rendering");
   }
+
+  self.collectStar = function(player, star) {
+    
+    star.kill();
+    self.score += 10;
+    self.scoreText.text = 'Score: ' + self.score;
+
+}
 
   
   
