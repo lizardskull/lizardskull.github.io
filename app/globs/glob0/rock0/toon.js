af07079bc70454f7a3d5bceeaac7795b11458ee9
brad henderson <elliotbradly@gmail.com>
2014-07-05 14:00:15 -0700

    i believe i have the solution

--- app/globs/glob0/rock0/toon.js
+++ app/globs/glob0/rock0/toon.js
@@ -3,10 +3,15 @@
 var Opening = function ( $glob )  { 
   
   var self       = Object.create( module, {});
+
   self.name = "Black",
   self.contentType = 'Chariot';
-  self.toonList = 	[   
-					{ viz:1,  	id:'toon',  src:"toon",  	 speed:1 },
+  self.toonList = [
+
+					{ viz:1,  	id:'toon1',  src:"toon",  	 speed:1 },
+					{ viz:1,  	id:'toon2',  src:"toon",  	 speed:1 },
+					{ viz:1,  	id:'toon3',  src:"toon",  	 speed:1 }
+
            			];
   return self; 
 };
